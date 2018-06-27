1085af3cb7d467b1877e2128cfb1c7560a570cda
Jennifer Colna <jennifer.colna@gmail.com>
2018-06-27 10:50:09 -0500

    added post request

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-
+'use strict';
 const express = require('express');
 const router = express.Router();
 const morgan = require('morgan');
@@ -47,6 +47,19 @@ app.post('/shopping-list', jsonParser, (req, res) => {
   res.status(201).json(item);
 });
 
+app.post('/recipes', jsonParser, (req, res)=>{
+  const requiredFields = ['name','ingredients'];
+  for (let i=0; i<requiredFields.length; i++){
+    const field = requiredFields[i];
+    if (!(field in req.body)){
+      const message = `Missing ${field} in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+  const recipe = Recipes.create(req.body.name, req.body.ingredients);
+  res.status(201).json(recipe);
+});
 
 app.get('/recipes', (req, res) => {
   res.json(Recipes.get());
